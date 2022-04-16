39a15ee3c859667392895103272718df187690fc
Júlio Pinto <a100742@alunos.uminho.pt>
2022-04-16 23:25:27 +0000

    feat: Guião 3 com ternary operator

--- code/logica.c
+++ code/logica.c
@@ -3,65 +3,64 @@
 #include "conversions.h"
 #include "stack.h"
 #include <math.h>
-#include <string.h>
 
 
-void lesser_then_long_operation(Stack *stack, long a, long b){
-    push_long(stack, a > b ? 1 : 0);
+void lesser_than_long_operation(Stack *stack, long a, long b){
+    push_long(stack, b > a);
 }
 
-void lesser_then_double_operation(Stack *stack, double a, double b){
-    push_long(stack, a > b ? 1 : 0);
+void lesser_than_double_operation(Stack *stack, double a, double b){
+    push_long(stack, b > a);
 }
 
-void lesser_then_operation(Stack *stack){
-    operate_promoting_number_type( stack, lesser_then_double_operation, lesser_then_long_operation);
+void lesser_than_operation(Stack *stack){
+    operate_promoting_number_type(stack, lesser_than_double_operation, lesser_than_long_operation);
 }
 
-void bigger_then_long_operation(Stack *stack, long a, long b){
-    push_long(stack, a < b ? 1 : 0);
+void bigger_than_long_operation(Stack *stack, long a, long b){
+    push_long(stack, b < a);
 }
-void bigger_then_double_operation(Stack *stack, double a, double b){
-    push_long(stack, a < b ? 1 : 0);
-}
-
-void bigger_then_operation(Stack *stack){
-    operate_promoting_number_type( stack, bigger_then_double_operation, bigger_then_long_operation);
+void bigger_than_double_operation(Stack *stack, double a, double b){
+    push_long(stack, b < a);
 }
 
-void equal_long_operation(Stack *stack, long a, long b){
-    push_long(stack, a == b ? 1 : 0);
-}
-void equal_double_operation(Stack *stack, double a, double b){
-    push_long(stack, a == b ? 1 : 0);
+void bigger_than_operation(Stack *stack){
+    operate_promoting_number_type(stack, bigger_than_double_operation, bigger_than_long_operation);
 }
 
-void equal_operation(Stack *stack){
-    operate_promoting_number_type( stack, equal_double_operation, equal_long_operation);
+void is_equal_long_operation(Stack *stack, long a, long b){
+    push_long(stack, a == b);
+}
+void is_equal_double_operation(Stack *stack, double a, double b){
+    push_long(stack, a == b);
 }
 
-void e_long_operation(Stack *stack, long a, long b){
+void is_equal_operation(Stack *stack){
+    operate_promoting_number_type(stack, is_equal_double_operation, is_equal_long_operation);
+}
+
+void and_long_operation(Stack *stack, long a, long b){
     push_long(stack, a && b ? b : 0);
 }
 
-void e_double_operation(Stack *stack, double a, double b){
+void and_double_operation(Stack *stack, double a, double b){
     push_double(stack, a && b ? b : 0);
 }
 
-void e_operation(Stack *stack){
-    operate_promoting_number_type(stack, e_double_operation, e_long_operation);
+void and_operation(Stack *stack){
+    operate_promoting_number_type(stack, and_double_operation, and_long_operation);
 }
 
-void ou_long_operation(Stack *stack, long a, long b){
+void or_long_operation(Stack *stack, long a, long b){
     push_long(stack, a ? a : b);
 }
 
-void ou_double_operation(Stack *stack, double a, double b){
+void or_double_operation(Stack *stack, double a, double b){
     push_double(stack, a ? a : b);
 }
 
-void ou_operation(Stack *stack){
-    operate_promoting_number_type( stack, ou_double_operation, ou_long_operation);
+void or_operation(Stack *stack){
+    operate_promoting_number_type( stack, or_double_operation, or_long_operation);
 }
 
 void lesser_value_long_operation(Stack *stack, long a, long b){
@@ -73,7 +72,7 @@ void lesser_value_double_operation(Stack *stack, double a, double b){
 }
 
 void lesser_value_operation(Stack *stack){
-    operate_promoting_number_type(stack, lesser_then_double_operation, lesser_then_long_operation);
+    operate_promoting_number_type(stack, lesser_value_double_operation, lesser_value_long_operation);
 }
 
 void bigger_value_long_operation(Stack *stack, long a, long b){
@@ -85,13 +84,17 @@ void bigger_value_double_operation(Stack *stack, double a, double b){
 }
 
 void bigger_value_operation(Stack *stack){
-    operate_promoting_number_type(stack, bigger_then_double_operation, bigger_then_long_operation);
+    operate_promoting_number_type(stack, bigger_value_double_operation, bigger_value_long_operation);
 }
 
-void if_then_else_long_operation(Stack *stack, long a, long b, long c){
-    push_long(stack, a ? b : c);
-}
 
-void if_then_else_double_operation(Stack *stack, double a, double b, double c){
+void if_then_else_operation(Stack *stack){
+
+    StackElement c = pop(stack);
+    StackElement b = pop(stack);
+    StackElement a = pop(stack);
+
+    bool cond = is_truthy(&a);
     
+    push(stack, cond ? b : c);
 }
