e026d454a44b3a1e2ed9937c73261bbe7eabec2e
Júlio Pinto <a100742@alunos.uminho.pt>
2022-04-18 17:02:16 +0100

    fix: just so i can pull

--- code/operations_storage.c
+++ code/operations_storage.c
@@ -9,7 +9,7 @@
  * Caso o operador não tenha correspondente operação esta devolverá NULL.
  */
 StackOperationFunction get_operation(char op[]) {
-    const static StackOperationTableEntry entries[] = {
+    const StackOperationTableEntry entries[] = {
             {"+",  add_operation},
             {"-",  minus_operation},
             {"*",  mult_operation},
