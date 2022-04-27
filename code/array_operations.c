23e86bffce600eac67eb2eda0ca30a8fafd1f90f
hramos.13 <a100644@alunos.uminho.pt>
2022-04-27 12:00:51 +0100

    feat: Começado operações dos arrays

--- code/array_operations.c
+++ code/array_operations.c
@@ -1,13 +1,14 @@
 #include "array_operations.h"
+#include <string.h>
 
-void size_range_operation(Stack *stack){
+void size_range_operation(Stack *stack) {
     StackElement x = pop(stack);
     ElementType x_type = x.type;
 
-    if (x_type == LONG_TYPE){
-        int array[x.content.long_value-1];
-        for (int i=0;i!= x.content.long_value; i++){
-            array[i]=i;
+    if (x_type == LONG_TYPE) {
+        int array[x.content.long_value - 1];
+        for (int i = 0; i != x.content.long_value; i++) {
+            array[i] = i;
         }
         //push_array(stack, array);
     }
