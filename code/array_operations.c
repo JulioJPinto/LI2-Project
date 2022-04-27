2252541c41f4e70f3af1c8db573d280c8a7eed03
hramos.13 <a100644@alunos.uminho.pt>
2022-04-27 12:00:09 +0100

    feat: Começado operações de arrays

--- code/array_operations.c
+++ code/array_operations.c
@@ -12,13 +12,27 @@ void size_range_operation(Stack *stack){
         //push_array(stack, array);
     }
     //else fazer push do tamanho do array
+    free_element(x);
 }
 
-void concat_x_times_string(Stack *stack){
-    StackElement y = pop(stack);
-    ElementType y_type = y.type;
+void concat_x_times_string(Stack *stack) {
+    StackElement string_element = pop(stack);
 
-    StackElement x = pop(stack);
-    ElementType x_type = y.type;
+    char *result = string_element.content.string_value;
+
+    long times = pop_long(stack);
+
+    for (int i = 0; i < times; ++i) {
+        strcat(result, result);
+    }
+
+    push_string(stack, result);
+
+    free_element(string_element);
+}
+
+void separate_string_by_substrings(Stack *stack) {
 
 }
+
+
