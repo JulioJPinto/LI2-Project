17a64987471a1170bc293167d67596e07014e605
Francisco Ferreira <a100660@alunos.uminho.pt>
2022-05-12 17:25:27 +0100

    feat: Execução de blocos

--- code/block_operations.c
+++ code/block_operations.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include "block_operations.h"
 #include "logger.h"
+#include "parser.h"
 
 int try_to_parse_block(Stack *stack, char *word) {
     size_t word_length = strlen(word);
@@ -16,4 +17,22 @@ int try_to_parse_block(Stack *stack, char *word) {
     push_block(stack, word);
 
     return 1;
+}
+
+StackElement execute_block(StackElement target_element, StackElement block_element, StackElement *variables) {
+    if (block_element.type != BLOCK_TYPE) PANIC("Trying to execute a non-block_element type (%d).", block_element.type)
+
+    Stack *temp_stack = create_stack(10);
+    push(temp_stack, target_element);
+
+    PRINT_DEBUG("Starting to execute block {%s}:", block_element.content.block_value)
+    tokenize_and_parse(temp_stack, variables, block_element.content.block_value);
+
+    if (length(temp_stack) == 1) PANIC("Block execution result is empty or has more than 1 element")
+
+    StackElement result = pop(temp_stack);
+
+    free_stack(temp_stack);
+
+    return result;
 }
--- code/block_operations.h
+++ code/block_operations.h
@@ -3,3 +3,5 @@
 #include "stack.h"
 
 int try_to_parse_block(Stack *stack, char *word);
+
+StackElement execute_block(StackElement target_element, StackElement block_element, StackElement *variables);
