da7b4e835891e1efa96d19a77a3d05a889ba8623
Júlio Pinto <a100742@alunos.uminho.pt>
2022-05-14 00:29:07 +0100

    fix: while operation para blocos

--- code/block_operations.c
+++ code/block_operations.c
@@ -216,30 +216,33 @@ void while_top_truthy_operation(Stack *stack, StackElement *variables) {
     Stack *storage_stack = create_stack(stack->capacity);
     Stack *result_stack = create_stack(stack->capacity);
 
-    int controller = 0;
 
-    for(int i = 0; is_truthy(&element) && length(stack) >= 1; i++) {
+    for(int i = 0; length(stack) && is_truthy(&element); i++) {
 
         if ( i != 0 ) {
             element = pop(stack);
         }
+        
+        if (is_truthy(&element)) {
+            StackElement transformed_element = pop(execute_block(element, block_element, variables));
 
-        StackElement transformed_element = pop(execute_block(element, block_element, variables));
-        push(storage_stack, transformed_element);
-        
-        controller = 1;
-        
+            push(storage_stack, transformed_element);
+        }
+
+        free_element(element);
+
     }
-    for(int i = 0; i < length(storage_stack) + 1; i++) {
+    for(int i = 0; i < length(storage_stack) + 1 && storage_stack->current_index != -1; i++) {
+
         StackElement new_element = pop(storage_stack);
+
         if (is_truthy(&new_element)) push(result_stack, new_element);        
     }
 
-    if (controller) push_all(stack, result_stack);
+    push_all(stack, result_stack);
 
 
     free_element(block_element);
-    free_element(element);
     free_stack(storage_stack);
     free_stack(result_stack);
     
