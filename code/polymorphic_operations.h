38bee12366e0469f5e44ee7625f32307c9dd0931
Fikiling <a100696@uminho.pt>
2022-05-15 19:31:35 +0100

    documentação do polymorphic_operations.h concluída

--- code/polymorphic_operations.h
+++ code/polymorphic_operations.h
@@ -3,81 +3,79 @@
 #include "stack.h"
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico asterisco '*'
- * caso o tipo seja "BLOCK_TYPE" realizará a operacão sobre as @param{variables}
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '*' associado ao tipo correspondente
+ * @param stack target
+ * @param variables
  */
 void asterisk_operation(Stack *stack, StackElement *variables);
 
+
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico til '~'
- * caso o tipo seja "BLOCK_TYPE" realizará a operacão sobre as @param{variables}
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '~' associado ao tipo correspondente
+ * @param stack target
+ * @param variables
  */
 void tilde_operation(Stack *stack, StackElement *variables);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico menor do que '<'
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '<' associado ao tipo correspondente
+ * @param stack target
  */
 void lesser_than_symbol_operation(Stack *stack);
+
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico maior do que '>'
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '>' associado ao tipo correspondente
+ * @param stack target
  */
 void bigger_than_symbol_operation(Stack *stack);
+
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico percentagem '%',
- * caso o tipo seja "BLOCK_TYPE" realizará a operacão sobre as @param{variables}
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
- */
-/**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico parênteses do lado esquerdo '('
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '(' associado ao tipo correspondente
+ * @param stack target
  */
 void open_parentheses_operation(Stack *stack);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico parênteses do lado direito ')'
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo ')' associado ao tipo correspondente
+ * @param stack target
  */
 void close_parentheses_operation(Stack *stack);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico igual '='
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '=' associado ao tipo correspondente
+ * @param stack target
  */
 void equal_symbol_operation(Stack *stack);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico barra '/'
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '/' associado ao tipo correspondente
+ * @param stack target
  */
 void slash_symbol_operation(Stack *stack);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico cardinal '#'
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '#' associado ao tipo correspondente
+ * @param stack target
  */
 void hashtag_symbol_operation(Stack *stack);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico percentagem '%'
- * caso o tipo seja "BLOCK_TYPE" realizará a operacão sobre as @param{variables}
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '%' associado ao tipo correspondente
+ * @param stack target
+ * @param variables
  */
 void parentheses_symbol_operation(Stack *stack, StackElement *variables);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico vírgula ','
- * caso o tipo seja "BLOCK_TYPE" realizará a operacão sobre as @param{variables}
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo ',' associado ao tipo correspondente
+ * @param stack target
+ * @param variables
  */
 void comma_symbol_operation(Stack *stack, StackElement *variables);
 
 /**
- * @brief a função verifica o "type" do elemento do @param{stack} a ser tratado para fazer a correspondente operação a esse específico tipo associada ao sinal gráfico dólar '$',
- * caso o tipo seja "BLOCK_TYPE" realizará a operacão sobre as @param{variables}
- * @param stack, onde vamos buscar o elemento para saber qual o seu tipo
+ * @brief Executa a operação que tem simbolo '$' associado ao tipo correspondente
+ * @param stack target
+ * @param variables
  */
 void dollar_symbol_operation(Stack *stack, StackElement *variables);
