a0e8cbcd3b7a12e914c76ae70fb15b5537a7eb32
Fikiling <a100696@uminho.pt>
2022-05-15 19:41:45 +0100

    passei só a documentação das funções que tavam no .c para o .h dos ficheiros string_operations

--- code/string_operations.c
+++ code/string_operations.c
@@ -1,9 +1,6 @@
 #include <string.h>
 #include "string_operations.h"
 
-/**
- * @brief Função que recebe a stack e caso encontre '"' insere no @param{*word} a string.
- */
 int parse_string(Stack *stack, char *word) {
     size_t word_length = strlen(word);
 
@@ -38,20 +35,12 @@ int string_compare_base(Stack *stack) {
     return return_value;
 }
 
-/**
- * @brief Função que compara se duas strings são iguais, caso sejam devolve 1, caso não devolve 0
- * @param stack A Stack onde vamos buscar os elementos para comparar
- */
 void string_compare_equal_operation(Stack *stack) {
     int return_value = string_compare_base(stack);
 
     push_long(stack, return_value == 0);
 }
 
-/**
- * @brief Função que compara se a string a esquerda é maior (a nível lexicográfico) que a string a direita, caso seja devolve 1, caso não devolve 0
- * @param stack A Stack onde vamos buscar os elementos para comparar
- */
 void string_compare_bigger_operation(Stack *stack) {
     int return_value = string_compare_base(stack);
 
@@ -59,20 +48,12 @@ void string_compare_bigger_operation(Stack *stack) {
 
 }
 
-/**
- * @brief Função que compara se a string a esquerda é menor (a nível lexicográfico) que a string a direita, caso seja devolve 1, caso não devolve 0
- * @param stack A Stack onde vamos buscar os elementos para comparar
- */
 void string_compare_smaller_operation(Stack *stack) {
     int return_value = string_compare_base(stack);
 
     push_long(stack, return_value < 0);
 }
 
-/**
- * @brief Função que compara se a string a esquerda é menor (a nível lexicográfico) que a string a direita e devolve a menor das duas
- * @param stack A Stack onde vamos buscar os elementos para comparar
- */
 void string_compare_smaller_value_operation(Stack *stack) {
     StackElement fst_element = pop(stack);
     StackElement snd_element = pop(stack);
@@ -83,10 +64,6 @@ void string_compare_smaller_value_operation(Stack *stack) {
     free_element(return_value < 0 ? snd_element : fst_element);
 }
 
-/**
- * @brief Função que compara se a string a esquerda é maior (a nível lexicográfico) que a string a direita e devolve a maior das duas
- * @param stack A Stack onde vamos buscar os elementos para comparar
- */
 void string_compare_bigger_value_operation(Stack *stack) {
     StackElement fst_element = pop(stack);
     StackElement snd_element = pop(stack);
--- code/string_operations.h
+++ code/string_operations.h
@@ -2,16 +2,40 @@
 
 #include "stack.h"
 
+/**
+ * @brief Função que recebe a stack e caso encontre '"' insere no @param{*word} a string.
+ */
 int parse_string(Stack *, char *);
 
+/**
+ * @brief Função que compara se duas strings são iguais, caso sejam devolve 1, caso não devolve 0
+ * @param stack A Stack onde vamos buscar os elementos para comparar
+ */
 void string_compare_equal_operation(Stack *stack);
 
+/**
+ * @brief Função que compara se a string a esquerda é menor (a nível lexicográfico) que a string a direita, caso seja devolve 1, caso não devolve 0
+ * @param stack A Stack onde vamos buscar os elementos para comparar
+ */
 void string_compare_smaller_operation(Stack *stack);
 
+/**
+ * @brief Função que compara se a string a esquerda é maior (a nível lexicográfico) que a string a direita, caso seja devolve 1, caso não devolve 0
+ * @param stack A Stack onde vamos buscar os elementos para comparar
+ */
 void string_compare_bigger_operation(Stack *stack);
 
+/**
+ * @brief Função que compara se a string a esquerda é menor (a nível lexicográfico) que a string a direita e devolve a menor das duas
+ * @param stack A Stack onde vamos buscar os elementos para comparar
+ */
 void string_compare_smaller_value_operation(Stack *stack);
 
+/**
+ * @brief Função que compara se a string a esquerda é maior (a nível lexicográfico) que a string a direita e devolve a maior das duas
+ * @param stack A Stack onde vamos buscar os elementos para comparar
+ */
 void string_compare_bigger_value_operation(Stack *stack);
+
 
 void search_substring_in_string_operation(Stack *stack);
